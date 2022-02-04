f6ef75055e2ff3300fd375cf237967d1604bad31
Arthur Kravchenko <arturkra92@gmail.com>
2022-02-04 02:58:01 +0200

    add task-10

--- js/task-09.js
+++ js/task-09.js
@@ -4,5 +4,5 @@ function getRandomHexColor() {
 const Btn = document.querySelector('.change-color');
 const span = document.querySelector('.color');
 Btn.addEventListener('click', () => {
-  span.textContent = document.body.style.backgroundColor = getRandomHexColor();;
+  span.textContent = document.body.style.backgroundColor = getRandomHexColor()
 });
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
+//Обозначаем области с которыми будем работать, создаем переменные
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const box = document.querySelector('#boxes');
+const input = document.querySelector('input');
+//Вешаем слушателя событий, вкладываем переменные функции как аргументы
+createBtn.addEventListener('click', createBoxes); 
+destroyBtn.addEventListener('click', destroyBoxes); 
+//Прописываем логику в функционале программы
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+
+function createBoxes(amount) {
+  amount = input.value; 
+  let counter = 0;
+  for (let i = 1; i < amount; i++) {
+    counter += 10; 
+    if (i > 0) {
+      const div = document.createElement('div');
+      div.style.width = 30 + 'px'; 
+      div.style.height = 30 + 'px';
+      div.style.backgroundColor = getRandomHexColor();
+      box.append(div);
+    }
+  }
+}
+
+function destroyBoxes() {
+  box.innerHTML = '';
+  input.value = '';
+  };
